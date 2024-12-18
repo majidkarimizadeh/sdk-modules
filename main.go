fa79419b559d998db1390d1336aba4b495ec0c9a
Majid Karimizadeh <m.karimizadeh@global.leaseweb.com>
2024-12-18 14:19:58 +0100

    test

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
 package main
 
+import (
+	_ "github.com/majidkarimizadeh/sdk-modules/abuse"
+	_ "github.com/majidkarimizadeh/sdk-modules/aggregationpack"
+	_ "github.com/majidkarimizadeh/sdk-modules/dedicatedserver"
+	_ "github.com/majidkarimizadeh/sdk-modules/dns"
+	_ "github.com/majidkarimizadeh/sdk-modules/invoice"
+	_ "github.com/majidkarimizadeh/sdk-modules/publiccloud"
+)
+
 func main() {}
